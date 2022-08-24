8a16b182901e94d40f89d310a6ac93fb5516ade2
rajramk12 <raj.ramk12@outlook.com>
2022-08-24 12:00:06 +0530

    modal show

--- app/assets/javascripts/posts/style.js
+++ app/assets/javascripts/posts/style.js
@@ -1,17 +1,6 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-
-// $(document).ready(function(){
-//   $('.navbar .dropdown').hover(function() {
-//       $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-//   }, function() {
-//       $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-//   });
-// })
-
 $(document).ready(function()
   {
+    message("loaded");
     if ($(".single-post-card").length) {
         // set a solid background color style
         if (mode == 1) {
@@ -61,18 +50,3 @@ function randomColor() {
   var color = colorSet[Math.floor(Math.random() * colorSet.length)];
   return color;
 }
-
-$(document).ready( function() {
-  // when a post is clicked, show its full content in a modal window
-  $("body").on( "click", ".single-post-card, .single-post-list", function() {
-    var posted_by = $(this).find('.post-content .posted-by').html();
-    var post_heading = $(this).find('.post-content h3').html();
-    var post_content = $(this).find('.post-content p').html();
-    var interested = $(this).find('.post-content .interested').attr('href');
-    $('.modal-header .posted-by').text(posted_by);
-    $('.loaded-data h3').text(post_heading);
-    $('.loaded-data p').text(post_content);
-    $('.loaded-data .interested a').attr('href', interested);
-    $('#myModal').modal('show');
-      });
-});
